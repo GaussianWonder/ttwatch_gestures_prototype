0c9a9afcba587dac9b6cfa789f58f64b08b91d3e
GaussianWonder <teo.virghi@yahoo.ro>
2020-10-03 23:34:52 +0300

    ttwatch gestures prototype

---
+++ gestures.js
@@ -0,0 +1,23 @@
+/**
+ * TODO: nice display of cursor movement
+ * TODO: array with cursor points (with smart data gathering, don't fill the ram uselessly)
+ * TODO: auto clear the cursor point buffer
+ * 
+ * TODO: basic gestures (down, up, left, right, diagonals, edges)
+ * TODO: support for any combination of gestures
+ * 
+ * TODO: complex gestures
+ */
+
+function setup() {
+  createCanvas(window.innerWidth, window.innerHeight);
+  pixelDensity(displayDensity());
+  
+  frameRate(60);
+}
+
+
+function draw() {
+  background(33);
+  
+}
